7a0ffe06e1b8440c1497258c6416213dc702ee0e
anand <anand@DESKTOP-10F855L>
2022-05-03 20:14:11 +0530

    Add project files.

---
+++ AkengiTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AkengiTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
